4a2c32778b4dfb039fdc3ba19208cd38b784538c
Yang Cai <yangcaiworking@gmail.com>
2023-10-09 18:01:52 -0500

    feat: Initialize Spring Boot project with essential dependencies

---
+++ src/main/kotlin/com/apiexercise/currencyconverter/CurrencyConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.apiexercise.currencyconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurrencyConverterApplication>(*args)
+}
